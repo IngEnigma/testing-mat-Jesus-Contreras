0078a956a5a40ac7171355e1527770ffd47c5667
IngEnigma <zs220199@estudiantes.uv.mx>
2024-10-27 19:16:51 -0600

    finished branch

--- src/app/problem-3/StonesOnTheTable.py
+++ src/app/problem-3/StonesOnTheTable.py
@@ -6,3 +6,11 @@ def contar_piedras(p, piedras):
             result += 1
 
     return result
+
+#p = int(input())
+#piedras = list(input())
+#result = 0
+#for i in range(1 , len(piedras)):
+#    if piedras[i] == piedras[i-1] :
+#       result+=1
+#print(result)
