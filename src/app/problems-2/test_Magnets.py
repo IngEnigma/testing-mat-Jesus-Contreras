0e2d42e6b1f570da98048a05d55d0d0f065fcf9d
IngEnigma <zs220199@estudiantes.uv.mx>
2024-10-27 18:35:05 -0600

    branch problem 2 finished

---
+++ src/app/problems-2/test_Magnets.py
@@ -0,0 +1,8 @@
+from Magnets import contar_grupos_imanes
+import pytest
+
+def test_1():
+    assert contar_grupos_imanes(6, ['10', '10', '10', '01', '10', '10']) == 3
+
+def test_2():
+    assert contar_grupos_imanes(4, ['01', '01', '10', '10']) == 2
