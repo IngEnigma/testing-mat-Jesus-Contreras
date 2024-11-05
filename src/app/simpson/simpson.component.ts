9886b49fc1805a8e232c21c6c6e5cbc456e98eb2
IngEnigma <159200281+IngEnigma@users.noreply.github.com>
2024-11-05 13:59:48 -0600

    Simpson (#42)
    * create branch simpson
    
    * fixed branch simpson
    
    * fixed coverage
    
    * it test fixed and 100% test fixed
    
    * intgration test added in simpson
    
    ---------
    
    Co-authored-by: IngEnigma <zs220199@estudiantes.uv.mx>

--- src/app/simpson/simpson.component.ts
+++ src/app/simpson/simpson.component.ts
@@ -1,19 +1,31 @@
 import { Component } from '@angular/core';
 import { Simpson } from '../classes/simpson';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-simpson',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './simpson.component.html',
   styleUrl: './simpson.component.css'
 })
 export class SimpsonComponent {
 
   simp: Simpson;
+  fx: any;
+  x0 = 0;
+  x1 = 0;
+  seg = 0;
+  error = 0;
+  result: number | null = null;
 
   constructor(){
     this.simp = new Simpson();
+  }
+
+  calculateSimp(fx: any, x0: number, x1: number, seg: number, error: number){
+    this.result = this.getArea(this.fx, this.x0, this.x1, this.seg, this.error);
   }
 
   getArea(fx: any, x0: number, x1: number, seg: number, error: number) {
