2c39e43a3151250f926a085446cc8ba95f16a0d6
IngEnigma <159200281+IngEnigma@users.noreply.github.com>
2024-11-05 01:41:49 -0600

    Media (#37)
    * Revert "Merge pull request #7 from IngEnigma/media"
    
    This reverts commit 3cf3c1c09d890736038fae25d7ab2ec0cf25b0a1, reversing
    changes made to fe24052d5d9632a001e90ffdb322264c099c2cfa.
    
    * create branch media #6
    
    * fixed branch media #6
    
    * it changed
    
    * intgration test added
    
    ---------
    
    Co-authored-by: IngEnigma <zs220199@estudiantes.uv.mx>

--- src/app/media/media.component.ts
+++ src/app/media/media.component.ts
@@ -1,19 +1,39 @@
 import { Component } from '@angular/core';
 import { Media } from '../classes/media';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Utilities } from '../classes/utilities';
 
 @Component({
   selector: 'app-media',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './media.component.html',
   styleUrl: './media.component.css'
 })
 export class MediaComponent {
 
   media : Media;
+  util : Utilities;
 
   constructor() {
     this.media = new Media();
+    this.util = new Utilities();
+  }
+
+  imputString: string = '';
+  result: number | null = null;
+
+  calculateMedia(arrayString: string): number | null {
+    const array = this.util.processArray(arrayString);
+    if(array){
+      this.result = this.media.calcularMedia(array);
+    }
+    return this.result;
+  }
+
+  getArray(arrayString: string): number[] | null {
+    return this.util.processArray(arrayString);
   }
 
   getMedia(array: number[]): number {
