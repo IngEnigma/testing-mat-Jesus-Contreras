06d27e91e3a8f2ad64ba480a1d7bcc92760d55df
IngEnigma <zs220199@estudiantes.uv.mx>
2024-11-05 01:39:18 -0600

    intgration test added

--- src/app/media/media.component.spec.ts
+++ src/app/media/media.component.spec.ts
@@ -1,10 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MediaComponent } from './media.component';
+import { By } from '@angular/platform-browser';
 
 describe('MediaComponent', () => {
   let component: MediaComponent;
   let fixture: ComponentFixture<MediaComponent>; 
-  let media = new MediaComponent();
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -21,13 +21,52 @@ describe('MediaComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it('Should return media = 550.6 with input 160, 591, 114, 229, 230, 270, 128, 1657, 624, 1503', async () => {
+    // Arrange
+    component.imputString = '160, 591, 114, 229, 230, 270, 128, 1657, 624, 1503';
+    fixture.detectChanges();
+    // Act
+    const button = fixture.debugElement.query(By.css('button'));
+    button.triggerEventHandler('click', null);
+    await fixture.whenStable();
+    fixture.detectChanges();
+    // Assert
+    const resultText = fixture.debugElement.query(By.css('p')).nativeElement.textContent;
+    expect(resultText).toContain('550.6');
+  });
+
+  it('Should return media = 60.32 with input 15.0, 69.9, 6.5, 22.4, 28.4, 65.9, 19.4, 198.7, 38.8, 138.2', async () => {
+    // Arrange
+    component.imputString = '15.0, 69.9, 6.5, 22.4, 28.4, 65.9, 19.4, 198.7, 38.8, 138.2';
+    fixture.detectChanges();
+    // Act
+    const button = fixture.debugElement.query(By.css('button'));
+    button.triggerEventHandler('click', null);
+    await fixture.whenStable();
+    fixture.detectChanges();
+    // Assert
+    const resultText = fixture.debugElement.query(By.css('p')).nativeElement.textContent;
+    expect(resultText).toContain('60.32');
+  });
+
   it('Should return media = 550.6 with [160, 591, 114, 229, 230, 270, 128, 1657, 624, 1503]', () => {
-    const result = media.getMedia([160, 591, 114, 229, 230, 270, 128, 1657, 624, 1503]);
+    const result = component.getMedia([160, 591, 114, 229, 230, 270, 128, 1657, 624, 1503]);
     expect(result).toBe(550.6);
   });
 
   it('Should return media = 60.32 with [15.0, 69.9, 6.5, 22.4, 28.4, 65.9, 19.4, 198.7, 38.8, 138.2]', () => {
-    const result = media.getMedia([15.0, 69.9, 6.5, 22.4, 28.4, 65.9, 19.4, 198.7, 38.8, 138.2]);
+    const result = component.getMedia([15.0, 69.9, 6.5, 22.4, 28.4, 65.9, 19.4, 198.7, 38.8, 138.2]);
     expect(result).toBe(60.32);
   });
+
+  it('Should return de string 160, 591, 114, 229, 230, 270, 128, 1657, 624, 1503 in to array', () => {
+    const result = component.getArray('160, 591, 114, 229, 230, 270, 128, 1657, 624, 1503');
+    expect(result).toEqual([160, 591, 114, 229, 230, 270, 128, 1657, 624, 1503]);
+  })
+
+  it('Should return de string 15.0, 69.9, 6.5, 22.4, 28.4, 65.9, 19.4, 198.7, 38.8, 138.2 in to array', () => {
+    const result = component.getArray('15.0, 69.9, 6.5, 22.4, 28.4, 65.9, 19.4, 198.7, 38.8, 138.2');
+    expect(result).toEqual([15.0, 69.9, 6.5, 22.4, 28.4, 65.9, 19.4, 198.7, 38.8, 138.2]);
+  })
+
 });
